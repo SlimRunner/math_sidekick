efacdc891ca788e5b1d3974cb827dfd6ca5b8667
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2019-06-21 21:36:44 -0700

    Added support for complex numbers

--- math_sidekick/gui/TrinomialsForm.cs
+++ math_sidekick/gui/TrinomialsForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Numerics;
+using math_sidekick.libraries;
 
 namespace math_sidekick
 {
@@ -25,41 +27,20 @@ namespace math_sidekick
             g.DrawBezier(pen, 0, 0, 50, 0, 0, 50, 50, 50);
 
             string answer;
-            double x1, x2;
-            double i1, i2;
-            double discriminant;
             double aTerm, bTerm, cTerm;
             
             double.TryParse(TermA_Input.Text, out aTerm);
             double.TryParse(TermB_Input.Text, out bTerm);
             double.TryParse(TermC_Input.Text, out cTerm);
 
-            discriminant = bTerm * bTerm - 4 * aTerm * cTerm;
+            Complex[] result = MyMath.FindRootsQuadTrinom(aTerm, bTerm, cTerm);
 
-            if (discriminant > 0)
-            {
-                //two real roots
-                x1 = (-bTerm + Math.Sqrt(discriminant)) / (aTerm * 2);
-                x2 = (-bTerm - Math.Sqrt(discriminant)) / (aTerm * 2);
-            }
-            else if (discriminant < 0)
-            {
-                //two complex roots
-                //discriminant = Math.Abs(discriminant);
-                //x1 = (-bTerm + Math.Sqrt(discriminant)) / aTerm * 2;
-                //x2 = (-bTerm - Math.Sqrt(discriminant)) / aTerm * 2;
-
-                x1 = double.NaN;
-                x2 = double.NaN;
-            }
-            else
-            {
-                //one root
-                x1 = (-bTerm + Math.Sqrt(discriminant)) / (aTerm * 2);
-                x2 = x1;
-            }
-
-            answer = "x1: " + x1 + Environment.NewLine + "x2: " + x2;
+            answer = string.Format(
+                "x1 = {0:0.###} + {1:0.###}i\nx2 = {2:0.###} + {3:0.###}i",
+                result[0].Real,
+                result[0].Imaginary,
+                result[1].Real,
+                result[1].Imaginary);
 
             MessageBox.Show(answer);
         }
--- math_sidekick/libraries/MyMath.cs
+++ math_sidekick/libraries/MyMath.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace math_sidekick.libraries
 {
@@ -26,6 +27,39 @@ namespace math_sidekick.libraries
         public static float Lerp(float value1, float value2, float t)
         {
             return (1 - t) * value1 + t * value2;
+        }
+
+        public static Complex[] FindRootsQuadTrinom(Complex a_term, Complex b_term, Complex c_term)
+        {
+            //all quadratic trinomials have two answers
+            Complex x1, x2;
+            Complex[] x = new Complex[]
+            {
+                new Complex(0, 0),
+                new Complex(0, 0)
+            };
+            Complex discriminant;
+
+            discriminant = Complex.Add(Complex.Pow(b_term, 2), Complex.Multiply(-4, Complex.Multiply(a_term, c_term)));
+
+            if (discriminant.Imaginary != Complex.Zero.Imaginary)
+            {
+                //idk wtf this is supposed to mean???
+                //return x;
+            }
+
+            if (discriminant.Real != 0)
+            {
+                x[0] = Complex.Divide(Complex.Add(Complex.Negate(b_term), Complex.Sqrt(discriminant)), Complex.Multiply(2, a_term));
+                x[1] = Complex.Divide(Complex.Subtract(Complex.Negate(b_term), Complex.Sqrt(discriminant)), Complex.Multiply(2, a_term));
+            }
+            else
+            {
+                x[0] = Complex.Divide(Complex.Add(Complex.Negate(b_term), Complex.Sqrt(discriminant)), Complex.Multiply(2, a_term));
+                x[1] = x[0];
+            }
+
+            return x;
         }
     }
 }
